3d41241d0590f023c0addaa8541035427831c5ee
Falkreon <falkreon@gmail.com>
2023-12-31 20:00:43 -0600

    Fix Arrow of Mutagen not applying mutagen

--- src/main/java/blue/endless/mutagen/MutagenStatus.java
+++ src/main/java/blue/endless/mutagen/MutagenStatus.java
@@ -2,24 +2,29 @@ package blue.endless.mutagen;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.InstantStatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.world.World;
 
-public class MutagenStatus extends StatusEffect {
+public class MutagenStatus extends InstantStatusEffect {
 
 	protected MutagenStatus() {
 		super(StatusEffectCategory.NEUTRAL, 0xFF_66FF66);
 	}
 	
 	@Override
-	public boolean isInstant() {
-		return true;
+	public void applyUpdateEffect(LivingEntity entity, int amplifier) {
+		applyEffect(entity);
 	}
 	
 	@Override
 	public void applyInstantEffect(Entity source, Entity attacker, LivingEntity target, int amplifier, double proximity) {
+		applyEffect(target);
+	}
+	
+	public void applyEffect(LivingEntity target) {
+		System.out.println("Applying effect to " + target.getClass().getSimpleName());
 		
 		if (target instanceof net.minecraft.entity.passive.SalmonEntity) {
 			World world = target.getWorld();
